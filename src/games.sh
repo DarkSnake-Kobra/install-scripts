2eb45bec5475ce2d655626b3722eeb91c33e2f4d
DarkSnake-Kobra <jbttoms@gmail.com>
2019-06-08 22:18:46 -0500

    more syntax fixes
    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

--- src/games.sh
+++ src/games.sh
@@ -53,4 +53,4 @@ apt-get install vanilla-gnome-settings -y
 echo "You will need to restart your pc and select 'GNOME on Xorg' from the gear menu below password entry box"
 echo "Be advised GNOME on Wayland is still experimental and some apps may not function properly."
 }
-menu()
+menu
